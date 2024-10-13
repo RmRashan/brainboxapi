e3cc57c361326f30386dc7395878260d45c331e0
Rm_Rashan <info.rmrashan>
2024-10-13 19:51:01 +0530

    ytfty

--- utils/DBConnection.js
+++ utils/DBConnection.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
+    port: process.env.PORT,
     password: process.env.PASSWORD,
     database: process.env.DATABSE
 });
